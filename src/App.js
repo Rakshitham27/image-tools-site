60778a5a36ebb0c0dccbdf707c70864a5353b89b
Rakshitham27 <rakshitham369@gmail.com>
2025-11-13 21:40:39 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,31 @@
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import MainPage from "./components/MainPage";
+// import BackgroundColorChanger from "./components/BackgroundColorChanger";
+// import ResizeImage from "./components/ResizeImage";
+// import ConvertToPDF from "./components/ConvertToPDF";
+// import AddText from "./components/AddText";
+// import CropImage from "./components/CropImage";
+// import CompressImage from "./components/CompressImage";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<MainPage />} />
+//         <Route path="/background-color" element={<BackgroundColorChanger />} />
+//         <Route path="/resize" element={<ResizeImage />} />
+//         <Route path="/convert-pdf" element={<ConvertToPDF />} />
+//         <Route path="/add-text" element={<AddText />} />
+//         <Route path="/crop" element={<CropImage />} />
+//         {/* <Route path="/compress" element={<CompressImage />} /> */}
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./components/MainPage";
@@ -10,7 +38,7 @@ import CompressImage from "./components/CompressImage";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/image-tools-site">
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/background-color" element={<BackgroundColorChanger />} />
